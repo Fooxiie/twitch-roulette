9fe92bb43f5e3bd13de80297f7a538f0cda7287f
Fooxiie <dev.fooxiie@gmail.com>
2022-04-18 23:49:07 +0200

    Retour du résultat sous forme html

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Bet;
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class GameController extends Controller
 {
 
-    public function test() {
+    public function test()
+    {
         return view('test');
     }
 
-    public function form_result_addbet(Request $request) {
+    public function form_result_addbet(Request $request)
+    {
         if ($this->addBet(1, $request->input('pseudo'), $request->input('amount'), $request->input('chiffre'))) {
             return redirect(route('test'));
         } else {
@@ -21,7 +25,8 @@ class GameController extends Controller
         }
     }
 
-    public function spinRoulette(Request $request) {
+    public function spinRoulette(Request $request)
+    {
         $game = Game::query()->find($request->query('game_id'))->get()[0];
         $num = rand(0, 37);
         $game->number = $num;
@@ -29,26 +34,47 @@ class GameController extends Controller
         return $num;
     }
 
-    public function verif_bet_for_game(Request $request) {
+    public function verif_bet_for_game(Request $request)
+    {
         $game = Game::query()->find($request->query('game_id'))->get()[0];
         $nbWinner = 0;
+        $html = "";
         foreach ($game->bets as $bet) {
             if ($bet->number != $game->number) {
+                $html .= "<div class='flex bg-red-500'>".$bet->viewer." loose ".$bet->amount." boxs !</div>";
                 $bet->winned = false;
             } else {
+                $html .= "<div class='flex bg-green'>".$bet->viewer." win ".$bet->amount." boxs !</div>";
                 $bet->winned = true;
                 $nbWinner = true;
             }
+            $bet->save();
+            $this->report_bet($bet);
         }
-        return $nbWinner;
+
+        $html = "<h3>Nombre de gagnant : {$nbWinner}</h3>".$html;
+
+        return $html;
     }
 
-    private function addBet($game_id, string $pseudo, int $amount, int $number) {
+    private function addBet($game_id, string $pseudo, int $amount, int $number)
+    {
         $bet = new Bet();
         $bet->game_id = $game_id;
         $bet->viewer = $pseudo;
         $bet->amount = $amount;
         $bet->number = $number;
         return $bet->save();
+    }
+
+    public function report_bet(Bet $bet)
+    {
+        $url = (($bet->winned) ?
+            'https://wapi.wizebot.tv/api/currency/' . Auth::user()->wizebot_key . '/action/add/' . $bet->viewer . '/' . $bet->amount
+            : 'https://wapi.wizebot.tv/api/currency/' . Auth::user()->wizebot_key . '/action/remove/' . $bet->viewer . '/' . $bet->amount . '/0');
+        $response = Http::post($url);
+        if ($response->status() != 200) {
+            dd($response, $url);
+        }
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,61 +1,60 @@
-@php
-use App\Models\Game;
-$game = Game::query()->find(1)->first();
-@endphp
+<x-guest-layout>
+    <form action="{{route('form.test')}}" method="get">
+        @csrf
+        <label>
+            Amount
+            <input type="text" name="amount" />
+        </label>
+        <label>
+            Pseudo
+            <input type="text" name="pseudo" />
+        </label>
+        <label>
+            Chiffre choisis
+            <input type="text" name="chiffre" />
+        </label>
+        <input type="submit" />
+    </form>
 
-<form action="{{route('form.test')}}" method="get">
-@csrf
-    <label>
-        Amount
-        <input type="text" name="amount" />
-    </label>
-    <label>
-        Pseudo
-        <input type="text" name="pseudo" />
-    </label>
-    <label>
-        Chiffre choisis
-        <input type="text" name="chiffre" />
-    </label>
-    <input type="submit" />
-</form>
+    <h3>Paris dans la game</h3>
+    @php
+        $game = App\Models\Game::query()->find(1)->first();
+    @endphp
+    <ul id="list bet">
+        @foreach($game->bets as $bet)
+            <li>From : {{$bet->viewer}} on {{$bet->number}} with {{$bet->amount}}</li>
+        @endforeach
+    </ul>
+    <button onclick="spinMe()">Spin fucking roulette</button>
+    <span id="coucou"></span>
 
-<h3>Paris dans la game</h3>
-<ul id="list bet">
-@foreach($game->bets as $bet)
-    <li>From : {{$bet->viewer}} on {{$bet->number}} with {{$bet->amount}}</li>
-@endforeach
-</ul>
-<button onclick="spinMe()">Spin fucking roulette</button>
-<span id="coucou"></span>
+    <button onclick="verifyBet()">Vérifier les paris</button>
+    <div id="result">
 
-<button onclick="verifyBet()">Vérifier les paris</button>
-<div id="result">
+    </div>
 
-</div>
-
-<script>
-    function spinMe() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-                document.getElementById("coucou").innerHTML =
-                    this.responseText;
-            }
-        };
-        xhttp.open("GET", '{{route('test.spin', ['game_id' => $game->id])}}', true);
-        xhttp.send();
-    }
-
-    function verifyBet() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                document.getElementById('result').append(this.responseText);
-                document.getElementById('')
-            }
+    <script>
+        function spinMe() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById("coucou").innerHTML =
+                        this.responseText;
+                }
+            };
+            xhttp.open("GET", '{{route('test.spin', ['game_id' => $game->id])}}', true);
+            xhttp.send();
         }
-        xhttp.open("GET", '{{route('test.verifbet', ['game_id' => $game->id])}}', true);
-        xhttp.send();
-    }
-</script>
+
+        function verifyBet() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById('result').innerHTML = this.responseText;
+                }
+            }
+            xhttp.open("GET", '{{route('test.verifbet', ['game_id' => $game->id])}}', true);
+            xhttp.send();
+        }
+    </script>
+</x-guest-layout>
